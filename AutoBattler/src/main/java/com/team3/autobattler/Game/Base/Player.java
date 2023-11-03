bbbbfc48eb72809f37a8c23befcfee2008044087
Collin <collin.cypret564@gmail.com>
2023-11-03 14:42:41 -0500

    Create Player.java

---
+++ AutoBattler/src/main/java/com/team3/autobattler/Game/Base/Player.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.team3.autobattler.Game.Base;
+
+/**
+ *
+ * @author colli
+ */
+public class Player {
+    Player()
+    {
+    int gold = 10;
+    int health = 5;
+    int turnNum = 1;
+    }
+    
+}
