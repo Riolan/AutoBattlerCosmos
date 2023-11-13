aa0bf4f82eb4b6cf3da9653f4b376ba4063fc42e
Rio <riolaney@att.net>
2023-11-12 19:53:59 -0600

    Slight update still broken shop

--- AutoBattler/src/main/java/com/team3/autobattler/Game/Factories/UnitFactory.java
+++ AutoBattler/src/main/java/com/team3/autobattler/Game/Factories/UnitFactory.java
@@ -8,12 +8,23 @@ import com.team3.autobattler.Game.Base.UnitType;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+
 
 public class UnitFactory 
 {
     
     static Map<String, UnitType> unitTypes = new HashMap<>();
 
+    private static UnitFactory unitfactory = null;
+        
+    public Unit getUnit() {
+        Random rand = new Random();
+        int key = rand.nextInt(units.size());
+        return units.get(key);
+    }
+    
+    
     public static UnitType getUnitType(String name, String ability)
     {
         UnitType result = unitTypes.get(name);
