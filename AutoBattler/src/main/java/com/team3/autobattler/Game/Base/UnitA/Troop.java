f3fd9e82c24d07cde2edb1f076c1ba5a22774d4a
Rio <riolaney@att.net>
2023-11-23 19:09:52 -0600

    Working on user interaction for shop.

--- AutoBattler/src/main/java/com/team3/autobattler/Game/Base/UnitA/Troop.java
+++ AutoBattler/src/main/java/com/team3/autobattler/Game/Base/UnitA/Troop.java
@@ -14,57 +14,115 @@ import java.util.Map;
  * @author Rio
  */
 public class Troop {
+    //private List<Unit> aggregate = new ArrayList<>();
     public static Map<Integer, List<Unit>> aggregate = new HashMap();
-
-        public void createUnit(int client_uuid, Unit newUnit) {
-
+    
+    
+    
+    // More so add unit
+    public void createUnit(int client_uuid, Unit newUnit) {
+        
         System.out.println("New unit associated with: " + client_uuid);
-
+        
         // Shared information from a UnitType
         UnitType type = UnitFactory.getUnitType(newUnit.getName(),
                 newUnit.getName(), newUnit.getCost());
         // Specific information for each unit
         Unit unit = new Unit(newUnit.getHealth(),
-                newUnit.getHealth(), 
+                newUnit.getAttack(), 
                 newUnit.getType());
-
+       
         // add unit to troop aggregate (collection of units)
         if (!aggregate.containsKey(client_uuid)) {
             aggregate.put(client_uuid, null);
         }
-
+        
         List<Unit> units = aggregate.get(client_uuid);
         if (units != null) {
             if (units.add(unit)) {
                 aggregate.put(client_uuid, units);
             }
         } else {
-
+            
             List<Unit> list_ = new ArrayList();
             list_.add(unit);
             aggregate.put(client_uuid, list_);
         }
     }
 
+    
+    
     public void createUnit(int client_uuid, int health, int attack, String name, String ability, int cost) {
+
         // Shared information from a UnitType
         UnitType type = UnitFactory.getUnitType(name, ability, cost);
         // Specific information for each unit
         Unit unit = new Unit(health, attack, type);
+       
+        // add unit to troop aggregate (collection of units)
         if (!aggregate.containsKey(client_uuid)) {
             aggregate.put(client_uuid, null);
         }
-
+        
         List<Unit> units = aggregate.get(client_uuid);
         if (units != null) {
             if (units.add(unit)) {
                 aggregate.put(client_uuid, units);
             }
         } else {
-
+            
             List<Unit> list_ = new ArrayList();
             list_.add(unit);
             aggregate.put(client_uuid, list_);
         }
-    } 
+    }
+    
+    /* Nullable */
+    public List<Unit> getUnits(int key) {
+        if (aggregate.containsKey(key)) {
+            return aggregate.get(key);
+        }
+        return null;
+    }
+    
+    
+    // remove a unit at x location.
+    public boolean removeUnit(int client_uuid, int pos) {
+        
+        System.out.println("Removing unit associated with: " + client_uuid);
+        
+        if (!aggregate.containsKey(client_uuid)) return false;
+        
+        List<Unit> units = aggregate.get(client_uuid);
+        if (units.size() - 1 < pos) {
+            return false;
+        }
+
+        units.remove(pos);
+        return true;
+    }
+    
+    
+    
+    // Swap unit at A location to B, and B to A.
+    public boolean swapUnits(int client_uuid, int A, int B) {
+       
+        if (!aggregate.containsKey(client_uuid)) return false;
+        
+        List<Unit> units = aggregate.get(client_uuid);
+        
+        if (units.size() < 2) return false;
+        
+        if (A < (units.size() - 1) || B < (units.size() - 1)) {
+            return false;
+        }
+        
+        Unit holdA = units.get(A);
+        Unit holdB = units.get(B);
+        units.set(A, holdB);
+        units.set(B, holdA);        
+        return true;
+    }
+    
+    
 }
--- AutoBattler/src/main/java/com/team3/autobattler/SceneManagement/Scenes/Shop.java
+++ AutoBattler/src/main/java/com/team3/autobattler/SceneManagement/Scenes/Shop.java
@@ -22,11 +22,33 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import com.team3.autobattler.SceneManagement.Scenes.PlayerPanel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.DropMode;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.TransferHandler;
+import static javax.swing.TransferHandler.COPY_OR_MOVE;
+import static javax.swing.TransferHandler.MOVE;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -36,15 +58,16 @@ import org.json.JSONObject;
  * @author colli
  */
 public class Shop extends javax.swing.JPanel {
-//    Don't care about items atm can implement later
-//    ItemFactory itemFactory = ItemFactory.getInstance();
-//    Item item = itemFactory.getItem();
-    
     final int MAX_UNITS = 5;
     Troop aTroop = new Troop();
     Player player = Player.getPlayer();
     List<PlayerPanel> listOfPlayerPanels = new ArrayList<PlayerPanel>();
     List<BuyPanel> listOfBuyPanels = new ArrayList<BuyPanel>();
+    
+    DefaultListModel from = new DefaultListModel();
+    DefaultListModel move = new DefaultListModel();
+    
+    JList dragFrom;
     
     byte attemptedBuy = 0b0000;
     
@@ -54,9 +77,148 @@ public class Shop extends javax.swing.JPanel {
     public Shop() {
         initComponents();
 
+        this.setLayout(new GridLayout(3, 3));       
+        
+        
+        
+        
+        for (int i = 15; i >= 0; i--) {
+            from.add(0, "Source item " + i);
+        }
+
+        for (int i = 2; i >= 0; i--) {
+            move.add(0, "Target item " + i);
+        }
+
+        // Drag panel
+        JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+        
+        dragFrom = new JList(from);
+        dragFrom.setTransferHandler(new customTransferHandler(TransferHandler.MOVE));
+        dragFrom.setPrototypeCellValue("List Item WWWWWW");
+        dragFrom.setDragEnabled(true);
+        dragFrom.setDropMode(DropMode.INSERT);
+        dragFrom.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JLabel label = new JLabel("Drag from here:");
+        label.setAlignmentX(0f);
+        p.add(label);
+        JScrollPane sp = new JScrollPane(dragFrom);
+        sp.setAlignmentX(0f);
+        p.add(sp);
+        add(p, BorderLayout.WEST);
+        
+        // Move
+        JList moveTo = new JList(move);
+        moveTo.setTransferHandler(new customTransferHandler(TransferHandler.MOVE));
+        moveTo.setPrototypeCellValue("List Item WWWWWW");
+        moveTo.setDropMode(DropMode.INSERT);
+        moveTo.setDragEnabled(true);
+        moveTo.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        // Copy Panel
+        p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+        sp = new JScrollPane(moveTo);
+        sp.setAlignmentX(0f);
+        p.add(sp);
+        label = new JLabel("Drop to MOVE to here:");
+        label.setAlignmentX(0f);
+        p.add(label);
+        add(p, BorderLayout.CENTER);       
     }
     
     
+    
+    class customTransferHandler extends TransferHandler {
+        int action;
+        private int index = 0;
+        
+        
+        public customTransferHandler(int action) {
+            this.action = action;
+        }
+        
+        public int getSourceActions(JComponent comp) {
+            return MOVE;
+        }
+
+
+
+        public Transferable createTransferable(JComponent comp) {
+            JList c = (JList)comp;
+            DefaultListModel model = (DefaultListModel)c.getModel();
+            index = c.getSelectedIndex();
+
+            if (index < 0 || (index >= model.getSize())) {
+                return null;
+            }
+
+            return new StringSelection((String)c.getSelectedValue());
+        }
+        
+        public void exportDone(JComponent comp, Transferable trans, int action) {
+            if (action != MOVE) {
+                return;
+            }
+            JList c = (JList)comp;
+            DefaultListModel model = (DefaultListModel)c.getModel();
+            model.removeElementAt(index);
+        }
+        
+        public boolean canImport(TransferHandler.TransferSupport support) {
+            // for the demo, we'll only support drops (not clipboard paste)
+            if (!support.isDrop()) {
+                return false;
+            }
+
+            // we only import Strings
+            if (!support.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                return false;
+            }
+
+            boolean actionSupported = (action & support.getSourceDropActions()) == action;
+            if (actionSupported) {
+                support.setDropAction(action);
+                return true;
+            }
+
+            return false;
+        }
+
+        public boolean importData(TransferHandler.TransferSupport support) {
+            // if we can't handle the import, say so
+            if (!canImport(support)) {
+                return false;
+            }
+
+            // fetch the drop location
+            JList.DropLocation dl = (JList.DropLocation)support.getDropLocation();
+
+            int index = dl.getIndex();
+
+            // fetch the data and bail if this fails
+            String data;
+            try {
+                data = (String)support.getTransferable().getTransferData(DataFlavor.stringFlavor);
+            } catch (UnsupportedFlavorException e) {
+                return false;
+            } catch (java.io.IOException e) {
+                return false;
+            }
+
+            JList list = (JList)support.getComponent();
+            DefaultListModel model = (DefaultListModel)list.getModel();
+            model.insertElementAt(data, index);
+
+            Rectangle rect = list.getCellBounds(index, index);
+            list.scrollRectToVisible(rect);
+            list.setSelectedIndex(index);
+            list.requestFocusInWindow();
+
+            return true;
+        }  
+    }
+
     
     public void recieveData(JSONArray units) {
         // Remove old input
